8f49c0f9237abbc5ebf6c4d0ce318aa9bfd9b8e1
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-06-09 12:35:31 +0300

    Create Board.cpp

---
+++ Board.cpp
@@ -0,0 +1,29 @@
+#define BOARD_H
+#ifndef BOARD_H
+#include "Board.hpp"
+
+
+using namespace std;
+
+
+
+namespace ariel
+{ 	// Defines a direction of a message on the board
+
+    void Board::post(unsigned  int a ,unsigned  int b , Direction c ,string const& d)
+    {
+      
+      
+    }
+    string Board::read(unsigned int a ,unsigned  int b , Direction c , int len)
+    {
+        od=0;
+        return "";
+    }
+    string Board::show() 
+    {
+        od=0;
+        return "";
+    }	
+};
+#endif
