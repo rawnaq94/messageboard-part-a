4905ba741d33a6b8d9297a4cb0cb3464e1254ba2
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-06-09 12:32:06 +0300

    Create Board.hpp

---
+++ Board.hpp
@@ -0,0 +1,23 @@
+#include "Direction.hpp"
+#include <string>
+#include <iostream>
+using std::string;
+
+
+
+
+
+namespace ariel
+{ // Defines a direction of a message on the board
+  
+	class Board 
+  {
+	    	private:
+		      	int od;
+        public:
+	  		Board(){}
+		    void post(unsigned int,unsigned int, Direction,string const&);
+        string read(unsigned int,unsigned int, Direction, int);
+        string show();	
+	};
+};
